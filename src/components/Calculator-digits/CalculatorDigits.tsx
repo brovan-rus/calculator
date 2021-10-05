09e1dd17b11752580591aaa844117e1c9bde2022
Konstantin Brovtsev <brovan@yandex.ru>
2021-10-05 23:34:25 +0300

    feat: add kyboard eneter listener

--- src/components/Calculator-digits/CalculatorDigits.tsx
+++ src/components/Calculator-digits/CalculatorDigits.tsx
@@ -5,30 +5,27 @@ import styles from './CalculatorDigits.module.css';
 import { Button } from '../Button';
 import { digits } from '../utils/constants';
 
-export const CalculatorDigits: FC = () => {
-  const [expression, setExpression] = React.useState('');
-  const handleButton = (digit: string) => {
-    setExpression(digit);
+interface ICalculatorDigitsProps {
+  onEnter: (digit: string) => void,
+  onReset: () => void,
+  onResult: () => void,
+}
+
+export const CalculatorDigits: FC<ICalculatorDigitsProps> = ({onEnter, onReset, onResult}) => {
+
+  const handleButtonClick = (digit: string) => {
+    onEnter(digit);
   };
 
-  const handleResultButton = () => {
-    console.log('resultButton');
-  };
-
-  const handleResetButton = () => {
-    console.log('resetButton');
-  };
-
-  console.log(expression);
   return (
     <div className={styles.container}>
       {digits.map((digit) => {
         if (digit === '=') {
-          return <Button symbol={digit} isWhite key={digit} onClick={handleResultButton} />;
+          return <Button symbol={digit} isWhite key={digit} onClick={onResult} />;
         } else if (digit === 'C') {
-          return <Button symbol={digit} key={digit} onClick={handleResetButton} />;
+          return <Button symbol={digit} key={digit} onClick={onReset} />;
         } else {
-          return <Button symbol={digit} key={digit} onClick={() => handleButton(digit)} />;
+          return <Button symbol={digit} key={digit} onClick={() => handleButtonClick(digit)} />;
         }
       })}
     </div>
--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,13 +1,28 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 
 import styles from './Calculator.module.css';
+import {digits} from "../utils/constants";
 import {CalculatorDigits} from "../Calculator-digits";
-
 export const Calculator: FC = () => {
+  const [expression, setExpression] = React.useState('');
+  const handleEnter = (digit: string) => setExpression(expression+digit);
+  React.useEffect(() => {
+    const handleKeyDown =  (e: KeyboardEvent) => {
+      const key = e.key;
+      if (digits.includes(key) && !((key === '=') || (key === 'C'))) {handleEnter(key)}
+      if (key === 'Escape') {handleReset()}
+      if (key === 'Enter') {handleResult()}
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {document.removeEventListener('keydown', handleKeyDown)}
+  }, [handleEnter]);
+
+  const handleReset = () => setExpression('');
+  const handleResult = () => console.log('result');
   return (
     <div className={styles.container}>
-      <div />
-      <CalculatorDigits />
+      <div><h2>{expression}</h2></div>
+      <CalculatorDigits onEnter={handleEnter} onReset={handleReset} onResult={handleResult}/>
     </div>
   )
 }
