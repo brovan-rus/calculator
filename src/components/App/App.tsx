861e35115b5038eb389181b3aa55c79b3fb26a3e
Konstantin Brovtsev <brovan@yandex.ru>
2021-10-07 14:12:28 +0300

    fix: homepage in package.json, readme.md

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,17 @@
 import { FC } from 'react';
+import {BrowserRouter} from "react-router-dom";
 
 import {Calculator} from "../Calculator";
 import styles from './App.module.css';
 
 export const App: FC = () => {
   return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <div className={styles.page}>
       <div className={styles.container}>
         <Calculator />
       </div>
     </div>
+    </BrowserRouter>
   )
 }
