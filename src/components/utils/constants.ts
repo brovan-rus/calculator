95e2007c64fd51669fcee2443c694f5b8dcbed80
Konstantin Brovtsev <brovan@yandex.ru>
2021-10-11 14:49:41 +0300

    refactor: change resign and adaptive values for coorect view on all displays

--- src/components/utils/constants.ts
+++ src/components/utils/constants.ts
@@ -1,3 +1,3 @@
 export const digits = ['C', ' ', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '00', '0', ',', '='];
-export const maxDigits = 14;
+export const maxDigits = 12;
 export const operators = [ '√', '%', '/', '-', '+', 'x'];
